ead6e22aae0ffb20abae91060560fecfcae3090a
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-19 13:42:48 +0300

    added delete account alert

--- lib/widgets/accounts/accounts_card.dart
+++ lib/widgets/accounts/accounts_card.dart
@@ -54,7 +54,7 @@ class _AccountCardState extends State<AccountCard> {
               children: [
                 // Account list tile
                 SizedBox(
-                  width: 150.w,
+                  width: widget.account.username.length >= 9 ? 220.w : 100.w,
                   child: ListTile(
                     title: Text(widget.account.username,
                         style: TextStyle(color: white, fontSize: 15.sp)),
@@ -80,11 +80,62 @@ class _AccountCardState extends State<AccountCard> {
                   color: CupertinoColors.systemRed,
                 ),
                 onPressed: () {
-                // delete account from hive box
-                  Provider.of<AccountProvider>(context,listen: false).deleteHive(widget.index);
+                  showAlertDialog(context);
                 },
               ),
             ),
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+
+    // set up the buttons
+    Widget cancelButton = TextButton(
+      child: Text("Cancel", style: const TextStyle(color: white)),
+      onPressed:  () {
+        Navigator.pop(context);
+      },
+    );
+    // delete button
+    Widget deleteButton = SizedBox(
+      width: 100.w,
+      height: 25.h,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(CupertinoColors.destructiveRed),
+          foregroundColor: MaterialStateProperty.all(CupertinoColors.white),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                )
+            )
+        ),
+        child: Text("Delete"),
+        onPressed:  () {
+          // delete account from hive box
+          Provider.of<AccountProvider>(context,listen: false).deleteHive(widget.index);
+          Navigator.pop(context);
+        },
+      ),
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      backgroundColor: black,
+      title: Text("Delete Account", style: TextStyle(color: CupertinoColors.systemRed)),
+      content: Text("Are you sure?", style: TextStyle(color: white)),
+      actions: [
+        cancelButton,
+        deleteButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
